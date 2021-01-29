b03d6eb8c944ee1c3c28ea00d783803c7f06b0bd
Medet <medetkanmamyrov2022@u.northwestern.edu>
2021-01-28 19:23:15 -0600

    small changes

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { BottomNavigation, DefaultTheme, PaperProvider } from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 4,
+  colors: {
+    ...DefaultTheme.colors,
+    text: '#23022E',
+    primary: '#573280',
+    accent: '#23022E',
+  },
+};
+
+const RestaurantsRoute = () => <Text>Restaurants</Text>;
+
+const CartRoute = () => <Text>Cart</Text>;
+
+const MyComponent = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'restaurants', title: 'Restaurants', icon: 'silverware-fork-knife' },
+    { key: 'cart', title: 'Cart', icon: 'cart' },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    restaurants: RestaurantsRoute,
+    cart: CartRoute,
+  });
+
+  return (
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View>
+       <MyComponent/>
+      </View>
+    </PaperProvider>
   );
 }
 
